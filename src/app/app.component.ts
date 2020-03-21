eb25f7072fb616f46fbeeb0a085fa1dd25bba589
unknown <vijay.srj@gmail.com>
2020-03-21 14:20:22 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import PouchDB from 'node_modules/pouchdb';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pouchdb-angular';
+
+
+  pouchdb: any;
+
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.pouchdb = new PouchDB("pouchform");
+
+  }
+
+  saveForm() {
+
+    var pouchform = {
+
+      _id: new Date().toISOString(),
+      name: this.pouchform.value.name,
+      emailid: this.pouchform.value.emailid
+    }
+
+
+    this.pouchdb.put(pouchform, function (result, error) {
+
+      console.log(result);
+
+      if (!error) {
+
+        alert("Pouch form saved successfully")
+      }
+    }
+    )
+  }
+
+  pouchform = this.formBuilder.group(
+
+    {
+      name: '',
+      emailid: ''
+    }
+  )
 }
