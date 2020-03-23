fdedda648b95c94d13d3ece5da8d7608a8d0cdde
unknown <vijay.srj@gmail.com>
2020-03-23 14:28:52 +0530

    "search pouch db"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { FormBuilder } from '@angular/forms';
 
 import PouchDB from 'node_modules/pouchdb';
 
+import PouchDBFind from 'node_modules/pouchdb-find';
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,13 +17,54 @@ export class AppComponent {
 
 
   pouchdb: any;
+  result: any;
 
 
   constructor(private formBuilder: FormBuilder) {
 
+
     this.pouchdb = new PouchDB("pouchform");
 
+    PouchDB.plugin(PouchDBFind);
+
+    this.pouchdb.createIndex(
+      {
+
+        index: { fields: ['name'] }
+      }
+    );
+
   }
+
+
+
+  async search() {
+
+
+
+    let records = await this.getForms(this.pouchform.value.searchName);
+
+    this.result = records.docs;
+
+
+  }
+
+
+  getForms(searchName) {
+
+
+    return this.pouchdb.find(
+
+      {
+        selector: {
+          name: searchName
+        }
+      }
+    )
+
+  }
+
+
 
   saveForm() {
 
@@ -26,7 +72,8 @@ export class AppComponent {
 
       _id: new Date().toISOString(),
       name: this.pouchform.value.name,
-      emailid: this.pouchform.value.emailid
+      emailid: this.pouchform.value.emailid,
+
     }
 
 
@@ -46,7 +93,9 @@ export class AppComponent {
 
     {
       name: '',
-      emailid: ''
+      emailid: '',
+      searchName: '',
+
     }
   )
 }
